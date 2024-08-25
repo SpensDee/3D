a8b5a5456588f596609f592914b57cb6d3fce14e
onepjob@gmail.com <onepjob@gmail.com>
2024-08-25 16:40:38 +0300

    preload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 const model3D = document.querySelector(".model-3D__img");
 const startFrame = 0;
 const endFrame = 150;
+const images = [];
 
-function updateFrame() {
-    // Рассчитываем текущий кадр на основе прокрутки страницы
-    let currentFrame = Math.floor(window.pageYOffset / 20);
-
-    // Ограничиваем текущий кадр в пределах от startFrame до endFrame
-    currentFrame = Math.max(startFrame, Math.min(currentFrame, endFrame));
-
-    // Обновляем источник изображения с учётом паддинга
-    model3D.src = `assets/render_${currentFrame.toString().padStart(5, '0')}.webp`;
+// Функция предварительной загрузки всех изображений
+function preloadImages() {
+    for (let i = startFrame; i <= endFrame; i++) {
+        const img = new Image();
+        img.src = `assets/render_${i.toString().padStart(5, '0')}.webp`;
+        images.push(img);
+    }
 }
+
+// Функция обновления кадра
+function updateFrame() {
+    let currentFrame = Math.floor(window.pageYOffset / 20);
+    currentFrame = Math.max(startFrame, Math.min(currentFrame, endFrame));
+    model3D.src = images[currentFrame].src;  // Используем предварительно загруженные изображения
+}
+
+// Предварительно загружаем все изображения перед инициализацией скролл-обработчика
+preloadImages();
 
 // Добавляем обработчик события прокрутки
 window.addEventListener("scroll", updateFrame);
